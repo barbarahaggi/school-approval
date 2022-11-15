a6027896d9126e6dc01d6a028a276f1a22fed05e
Barbara Haggi <barbarahaggi3@gmail.com>
2022-11-15 13:30:14 +0000

    school approval

---
+++ script.js
@@ -0,0 +1,12 @@
+var nome = "barbara"
+var notaDoPrimeiroBimestre = 9
+var notaDoSegundoBimestre = 8
+var notaDoTerceiroBimestre = 5
+var notaDoQuartoBimestre = 9
+
+var notaFinal = (notaDoPrimeiroBimestre + notaDoSegundoBimestre + notaDoTerceiroBimestre + notaDoQuartoBimestre) / 4
+
+var notaFixada = notaFinal.toFixed(0)
+
+console.log ("bem vindo " + nome)
+console.log(notaFinal)
